ca96aa03a2049a016a3d05d8f253a935e2b5caf9
bleze564 <153006729+bleze564@users.noreply.github.com>
2024-10-25 15:41:10 +0300

    123123

---
+++ HW4/script.js
@@ -0,0 +1,55 @@
+let fieldOne = "text";
+let fieldTwo = "";
+
+if (fieldOne !== "" && fieldTwo !== ""){
+    alert("обидва поля заповннені");
+} else {
+    alert("нне всі поля заповнені")
+}
+
+
+
+const numOne =5;
+const numTwo = 7;
+const sum = numOne + numTwo
+
+if (sum > 10){
+    console.log("Сума більше за 10");
+} else {
+    console.log("Сума менша або дорівнює 10")
+}
+
+
+
+const text = "Ми вивчаємо мову програмуванння JavaScript"
+
+if(text.includes("JavaScript")){
+    console.log("текст містить слово")
+}else {
+    console.log("текст не містить слово")
+}
+
+
+
+
+const number =14;
+
+if (number > 10 && number < 20){
+    console.log("число входить в діапазонн")
+} else {
+    console.log("число нне входить в діапазон")
+} 
+
+
+
+
+const name ="Bob";
+const email = "bob@gmail.com";
+const pasword = "pasword1234";
+
+
+if(name.length >=3 && email.includes("@") && email.includes(".") && pasword.length >=6){
+    console.log("Перенаправлення на іншу сторіннку")
+} else{
+    console.log("Помилка:неправельне заповнення")
+}
