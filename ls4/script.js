e963ad096ff4ecf4cb74a251fd821b54ad807b76
bleze564 <153006729+bleze564@users.noreply.github.com>
2024-10-25 14:58:46 +0300

    312321

---
+++ ls4/script.js
@@ -0,0 +1,23 @@
+
+
+// const jsName =prompt(
+//    'яка назва джаваскріпту' 
+   
+// );
+// alert(
+//     jsName === 'ECMAScript' ? 'Правильно!' : 'Ви не знаєте? ECMAScript'
+// )
+
+
+
+
+// '?':
+// let result;
+// if (a + b < 4) {
+//   result = 'Нижче';
+// } else {
+//   result = 'Вище';
+// }
+
+
+const result = (a + b < 4) ? 'Нижче' : 'Вище';
