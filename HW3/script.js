72acc6b6cc0eb6437c3a4977bb8fb2237a364802
bleze564 <153006729+bleze564@users.noreply.github.com>
2024-11-11 19:54:33 +0200

    3123123

---
+++ HW3/script.js
@@ -0,0 +1,19 @@
+const result = 5+5+'5';
+console.log(result)
+
+
+const email ='email@12.34.com';
+console.log(email.includes('@'))
+console.log(email.length)
+
+
+const a ='My ';
+const b ='name ';
+const c ='is ';
+const fullName =a+b+c+'Viktor';
+console.log(fullName)
+
+
+const name ='Валерій';
+const cost =300;
+console.log('Дякуємо, '+name+'! До сплати '+cost+' гривень')
