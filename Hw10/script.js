ab1252af7796a3eada1fbc410fd9bff63246df7d
bleze564 <153006729+bleze564@users.noreply.github.com>
2024-12-19 17:25:20 +0200

    312313

---
+++ Hw10/script.js
@@ -0,0 +1,43 @@
+const processArray = (array, callback) => { 
+}; 
+const getSum = (arr) => (
+let sum =0;
+for (let num of arr){
+    sum += num;
+}
+return sum
+)
+
+const getMin = (arr) => {return Math.min (...arr)} 
+const getMax = (arr) => {return Math.max(...arr)}
+
+const array1 =[1,2,3,4,5];
+const array2 =[-15, 0, 36, -29, 125];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const operate = (a, b, callback) => { 
+}; 
+
+
+const add = (a, b) => a + b
+const subtract = (a,b) => a - b
+const multiply = (a,b) => a*b
+const 
+// Виклик функції з різними операціями 
+console.log(operate(10, 5, function(a, b){ return a - b})); // Віднімання 
+console.log(operate(10, 5, function(a, b){ return a + b})); // Додавання 
+console.log(operate(10, 5, function(a, b){ return a * b})); // Множення 
+console.log(operate(10, 5, function(a, b){ return a / b})); // Ділення
