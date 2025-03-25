374125922ae5677a650c23c11189cfac340a88ce
bleze564 <153006729+bleze564@users.noreply.github.com>
2025-03-25 19:40:40 +0200

    `121`

--- DOM/script.js
+++ DOM/script.js
@@ -4,5 +4,23 @@
 // console.log(firstItemEl)
 // const listElOne =firstItemEl.parentElement
 // console.log(listElOne)
-const imgEl =document.querySelector(".hero__image")
-console.dir(imgEl)
+// const imgEl =document.querySelector(".hero__image")
+// console.dir(imgEl)
+
+
+
+const articleEl = document.querySelector(".js-container");
+const product ={
+    name: 'Товар-2',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa eius deleniti dolor facilis consectetur laborum aperiam exercitationem quam hic natus.',
+    price: 3000,
+    available: false,
+    onSale: false,
+  }
+articleEl.innerHTML = `<article class="product">
+<h2 class="product__name">${product.name}</h2>
+<p class="product__descr">${product.description}</p>
+<p product__pridct>${product.name}</p>
+</article>`;
+
