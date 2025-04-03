66752a50a54935366efe2210ae68a5e7e16a616e
bleze564 <153006729+bleze564@users.noreply.github.com>
2025-04-03 19:54:34 +0300

    2331

--- modal/script.js
+++ modal/script.js
@@ -1,10 +1,31 @@
-// const formEl = document.querySelector('js-register-form')
+const closeButton = document.querySelector('[data-action="close-modal"]');
+const openButton = document.querySelector('[data-action="open-modal"]');
 
-// formEl.addEventListener('submit', onSubmit)
+closeButton.addEventListener("click", toggleModal);
+openButton.addEventListener("click", toggleModal);
 
-// function onSubmit
+function toggleModal() {
+  document.body.classList.toggle("show-modal");
+}
 
 
 
-const openModal =document.querySelector{"[data-action='open-modal']"}
-const closeModal =document.querySelector{'[data-action="close-modal"z`]'}
+// const openM = document.querySelector('[data-action="open-modal"]')
+// const backdrop = document.querySelector(".js-backdrop")
+// const modal = document.querySelector(".modal")
+// const body = document.body
+// const closeM = document.querySelector('[data-action="close-modal"]')
+
+// function onBtnClick() {
+//     document.body.classList.toggle('show-modal')
+
+// }
+// openM.addEventListener('click', onBtnClick);
+// closeM.addEventListener('click', onBtnClick);
+
+// window.addEventListener('keydown', (event) => {
+//     //console.log(event.key);
+//     if (event.key === 'Escape' && document.body.classList.contains('show-modal')) {
+//         onBtnClick()
+//     }
+// })
