12b0bce9e27fd4d2762806be94ac7c37837fb4b7
bleze564 <153006729+bleze564@users.noreply.github.com>
2025-09-02 19:44:53 +0300

    235

---
+++ 4symestr/synhrone/index.js
@@ -0,0 +1,7 @@
+// 2. Завдання "Лічильник": Створіть лічильник, який збільшується на одиницю кожну секунду. Виводьте значення лічильника на сторінці.
+const counterEL = document.querySelector('.counter')
+let counter = 0;
+setInterval(() => {
+    counter += 1;
+    counterEL
+}, );
