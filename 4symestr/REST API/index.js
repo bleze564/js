24d3878a1db87ecc7831bd79e6b48514eb2af48f
bleze564 <153006729+bleze564@users.noreply.github.com>
2025-09-16 19:52:25 +0300

    123

--- 4symestr/REST API/index.js
+++ 4symestr/REST API/index.js
@@ -1,9 +1,24 @@
-const formEL = document.querySelector('form')
-formEL.addEventListener('submit' ,  (ev)=>( 
-    ev.preventDefault()
-    formEL.reset()
-))
-function searchArticlesByName(querry){
-fetch('https://newsapi.org/docs/endpoints/everythinghttps://newsapi.org/v2/everything?q=${querry}&apiKey=3ce63eea477043d7a470d2b21dc5ab4b').then(result => result.json())
+const formEl = document.querySelector("form");
+const markupDiv =document.querySelector('.container')
+formEl.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  searchArticlesByName(ev.currentTarget.elements.query.value).then( (responds) => {
+   let markup =responds.articles.map(createArticleMarkup)
+   markupDiv.innerHTML = markup
+  })
+    formEl.reset();
+});
+function searchArticlesByName(news) {
+  return fetch(
+    `https://newsapi.org/v2/everything?q=${news}&apiKey=4cb0d0900a824cada0e90e272a031922`
+  ).then((result) => result.json());
 }
-
+function createArticleMarkup (article) {
+  return `
+       <article>
+        <img src="${article.urlToImage}" alt="${article.title}">
+        <h2>${article.title}</h2>
+        <p>${article.description}</p>
+     </article>
+`;
+}
